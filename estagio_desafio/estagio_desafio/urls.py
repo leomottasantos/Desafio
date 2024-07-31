6b7d2c5ff6e86a71d629fc2d8cbadb1a1b760f2b
Leonardo Motta <leonardomottadossantos@gmail.com>
2024-07-31 17:29:26 -0300

    docker

--- estagio_desafio/estagio_desafio/urls.py
+++ estagio_desafio/estagio_desafio/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.http import HttpResponse
 
 urlpatterns = [
     path('', include('usuario.urls')),
     path('admin/', admin.site.urls),
+    path('', lambda x: HttpResponse('Django e docker'))
 ]
